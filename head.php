2e7ecb7dd9ddf6ba111c2f9fa1cb98ecbac963cb
Claudiu <claudiu.simion.cornea@gmail.com>
2018-08-29 12:24:27 +0200

    head

---
+++ head.php
@@ -0,0 +1,10 @@
+<!DOCTYPE HTML>
+<html lang = "fr">
+    <head>
+        <title>COGIP</title>
+        <meta charset = "UTF-8">
+        <meta name = "description" content = "Plateforme de Gestion">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name = "author" content = "MAMALE Liliane / TAHRI Mostapha / CORNEA Claudiu">
+        <link href = "https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.1/css/bulma.min.css">
+    </head>
--- header.php
+++ header.php
@@ -1,9 +1,4 @@
-<!DOCTYPE HTML>
-<html lang = "fr">
-    <head>
-        <title>COGIP</title>
-        <meta charset = "UTF-8">
-        <meta name = "description" content = "Plateforme de Gestion">
-        <meta name = "author" content = "MAMALE Liliane / TAHRI Mostapha / CORNEA Claudiu">
-        <link href = "https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.1/css/bulma.min.css">
-    </head>
+<body>
+    <header>
+        
+    </header>
